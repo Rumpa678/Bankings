1d4998bcaeae4ac84bbbc064f518550a83e76029
Rumpa678 <rumpadas5991@gmail.com>
2023-06-01 13:28:08 +0530

    commitmess

--- src/test/java/practiceset/DemoTest.java
+++ src/test/java/practiceset/DemoTest.java
@@ -1,5 +1,14 @@
 package practiceset;
 
+import org.testng.annotations.Test;
+
 public class DemoTest {
+	@Test
+	public void get() {
+		System.out.println("hello i am fine");
+	}
+	public void hate() {
+		System.out.println("hello i am not fine");	
+	}
 
 }
