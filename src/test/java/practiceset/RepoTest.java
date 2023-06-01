98c424228f7130d18d69acd38ed1d398897aefde
Rumpa678 <rumpadas5991@gmail.com>
2023-06-01 15:29:37 +0530

    commited

---
+++ src/test/java/practiceset/RepoTest.java
@@ -0,0 +1,12 @@
+package practiceset;
+
+import org.testng.annotations.Test;
+
+public class RepoTest {
+	@Test
+	public void hate() {
+		System.out.println("hello i am not fine");
+		//something need to do
+	}
+
+}
